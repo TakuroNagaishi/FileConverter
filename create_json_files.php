fe1a93221959546d952c58a3c580b22db42170c5
Takuro-Nagaishi <takuro.nagaishi@mail.glow-a-culture.com>
2022-08-04 18:02:22 +0900

    cmt

--- create_json_files.php
+++ create_json_files.php
@@ -6,17 +6,24 @@ const OUTPUT_FILE_PATH = __DIR__ . '/Storage/Outputs/';
 const OUTPUT_FILE_PREFIX = 'testcase_';
 const OUTPUT_FILE_EXTENSION = '.json';
 
-
-$input_file_path = $argv[1];
-
-$file = new File($input_file_path);
+$file = new File($argv[1]);
 
 $values = $file->readAsCells();
 
 $header = $values[0];
 unset($values[0]);
 
+$cases = count($values);
+$suffix_length = strlen(strval($cases));
+
+function padWithZero(string $str, int $len): string
+{
+    return str_pad($str, $len, '0', STR_PAD_LEFT);
+}
+
 foreach ($values as $ind => $value) {
     $rtn = array_combine($header, $value);
-    file_put_contents(OUTPUT_FILE_PATH . OUTPUT_FILE_PREFIX . $ind . OUTPUT_FILE_EXTENSION, json_encode($rtn, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    $case = padWithZero($ind, $suffix_length);
+
+    file_put_contents(OUTPUT_FILE_PATH . OUTPUT_FILE_PREFIX . $case . OUTPUT_FILE_EXTENSION, json_encode($rtn, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
 }
