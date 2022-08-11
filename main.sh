fa36b4ed21d3f64d1275b276571def9a20aa52da
TakuroNagaishi <takuro.nagaishi@gmail.com>
2022-08-11 14:59:01 +0900

    add comment

--- main.sh
+++ main.sh
@@ -2,7 +2,11 @@
 
 cd `dirname $0`
 
+# Initialize output directry
 rm -r Storage/Outputs/
 mkdir -p Storage/Outputs/
 
+# Execute
 php create_json_files.php Storage/Inputs/resource.csv
+
+echo -e "\e[32mConverting was completed successfully.\e[m"
